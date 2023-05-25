4a8e8680e73d4d87d2944c346f6cb3bec8b2ffd8
Mate Kamaras programming <matekamarasprogramming@mates-air.localdomain>
2023-05-25 09:04:11 +0200

    declared an interface to define a template for an object

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,15 @@
 "use client" // this is a client component
 import React from "react"
+import { useState, useEffect } from "react"
+import { Link } from "react-scroll/modules"
+import { useTheme } from "next-themes"
+import { RiMoonFill, RiSunLine } from "react-icons/ri"
+import { IoMdMenu, IoMdClose } from "react-icons/io"
 
+interface NavItems {
+    label: string
+    page: string
+}
 
 const Navbar = () => {
     return (
