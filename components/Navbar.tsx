09ee20bed20263f3ec56b0fa0182dab11bfd25e0
Mate Kamaras programming <matekamarasprogramming@Mates-MacBook-Air.local>
2023-05-25 20:53:28 +0200

    add HeroSectionp

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,25 +6,114 @@ import { useTheme } from "next-themes"
 import { RiMoonFill, RiSunLine } from "react-icons/ri"
 import { IoMdMenu, IoMdClose } from "react-icons/io"
 
-interface NavItems {
+interface NavItem {
     label: string
     page: string
 }
 
-const NAV_ITEMS: Array<NavItems> = [
-    { label: "Home", page: "home" },
-    { label: "About", page: "about", },
-    { label: "Projects", page: "projects", },
+const NAV_ITEMS: Array<NavItem> = [
+    {
+        label: "Home",
+        page: "home",
+    },
+    {
+        label: "About",
+        page: "about",
+    },
+    {
+        label: "Projects",
+        page: "projects",
+    },
 ]
 
-const Navbar = () => {
 
-    const { systemTheme, theme, setTheme } = useTheme()
-    const currentTheme = theme === "system" ? systemTheme : theme
-    const [navbar, setNavbar] = useState(false)
+
+const NavBar = () => {
+    const [initialRender, setInitialRender] = useState(false); // Set initialRender to false
+
+    useEffect(() => {
+        setInitialRender(false);
+    }, []);
+
+    const { systemTheme, theme, setTheme } = useTheme();
+    const currentTheme = initialRender ? "dark" : theme === "system" ? systemTheme : theme;
+    /* const currentTheme = theme === "system" ? systemTheme : theme; */
+
+    const [navbar, setNavbar] = useState(false);
+
+
+
     return (
-        <div>Navbar!</div>
+        <header className="w-full  mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow  bg-white dark:bg-stone-900  dark:border-b dark:border-stone-600">
+
+            <div className="justify-between md:items-center md:flex">
+
+
+
+                <div>
+                    <div className="flex items-center justify-between py-3">
+                        <Link
+                            to="home"
+                            className={
+                                "block lg:inline-block text-neutral-900  hover:text-neutral-500  dark:text-teal-700 cursor-pointer"
+                            }
+                            activeClass="active"
+                            spy={true}
+                            smooth={true}
+                            offset={-100}
+                            duration={500}
+                            onClick={() => setNavbar(!navbar)}>
+                            <div className="md:py-5 md:block">
+                                <h2 className="text-2xl font-bold">Mate Kamaras</h2>
+                            </div>
+                        </Link>
+
+                        <div className="md:hidden">
+                            <button onClick={() => setNavbar(!navbar)}>{navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}</button>
+                        </div>
+                    </div>
+
+                </div>
+                <div>
+                    <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? "block" : "hidden"
+                        }`}>
+                        <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                            {NAV_ITEMS.map((item, idx) => {
+                                return (
+                                    <Link
+                                        key={idx}
+                                        to={item.page}
+                                        className={
+                                            "block lg:inline-block text-neutral-900  hover:text-neutral-500 hover:border-b-2 hover:border-neutral-200 dark:text-teal-700 cursor-pointer"
+                                        }
+                                        activeClass="active"
+                                        spy={true}
+                                        smooth={true}
+                                        offset={-100}
+                                        duration={500}
+                                        onClick={() => setNavbar(!navbar)}
+                                    >
+                                        {item.label}
+                                    </Link>
+                                )
+                            })}{
+                                currentTheme === "dark" ? (
+                                    <button onClick={() => setTheme("light")}
+                                        className="bg-slate-100 p-2 rounded-xl hover:translate-y-1 transition-transform">{<RiSunLine size={25} color="black" />}</button>
+                                ) : (
+                                    <button onClick={() => setTheme("dark")}
+                                        className="bg-slate-100 p-2 rounded-xl hover:translate-y-1 transition-transform"
+                                    >{<RiMoonFill />}</button>
+                                )
+                            }
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </header>
+
     )
 }
-
-export default Navbar
+export default NavBar;
