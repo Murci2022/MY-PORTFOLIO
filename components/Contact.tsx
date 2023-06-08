4c87a07386ae9513578a333bc8bab957d206cf83
Mate Kamaras programming <matekamarasprogramming@mates-air.localdomain>
2023-06-08 12:17:02 +0200

    add h1 Contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <section id='more' >Contact</section>
+    <section id='more' className='h-screen flex justify-center items-center'>
+        <div >
+        <h1 className='text-center text-red-500 mb-6 font-bold text-2xl pt-20 pb-5'> - Contact -</h1>
+        </div>
+    </section>
   )
 }
 
