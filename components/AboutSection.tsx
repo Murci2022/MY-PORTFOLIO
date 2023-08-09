eef9f52d05067def4849f59f29a79ff67a11abf3
MK <Javascript@mates-air.localdomain>
2023-08-09 12:19:31 +0200

    debugging

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Image from 'next/image'
-import TechStack from './TechStack'
+import React from 'react';
+import Image from 'next/image';
+import TechStack from './TechStack';
 
 /* const skills = [
     { skill: "HTML" },
@@ -16,107 +16,118 @@ import TechStack from './TechStack'
 
 ] */
 
-
 const AboutSection = () => {
-    /* */
-    return (
-        <section id="more" >
-            <div >
-                {/*  <h1 className=' text-center font-bold text-2xl text-red-500 mb-4'>
+  /* */
+  return (
+    <section id='more'>
+      <div>
+        {/*  <h1 className=' text-center font-bold text-2xl text-red-500 mb-4'>
                     - About Me -
                 </h1> */}
-            </div>
+      </div>
 
-            <div className="  break-normal bg-stone-100  dark:bg-black text-gray-800 dark:text-gray-300 py-3 px-3 rounded-xl">
-
-
-
-
-                {/* <div>
+      <div className='  break-normal bg-stone-100  dark:bg-black text-gray-800 dark:text-gray-300 py-3 px-3 rounded-xl'>
+        {/* <div>
                  <h1 className='text-center font-bold text-xl text-red-500'>
                 - About Me -</h1> 
 
 
                 <hr className=' w-6 h-1 mx-auto my-4  border-0 rounded'></hr></div>*/}
-                <div className="my-2 mx-4  md:pt-30 md:pb-30">
-
-
-                    <div className='flex flex-col space-y-10 items-stretch justify-center align-top  md:flex-row md:text-left md:p-4 md:space-y-0 md:space-x-10 '>
-                        <div className='md:w-1/2'>
-                            <h1 className='text-xl2 font-bold mb-6 md:text-left'>Hello there!</h1>
-                            <p> My name is{" "}
-                                <span className='font-bold'>{"Mate Kamaras"}</span>, and I'm a{" "}
-                                <span className='font-bold'>{"Frontend Developer "}</span>, who absolutely loves what I do. Creating awesome digital experiences that blend art, functionality, and user-friendly design is simply awesome{" "}
-
-
-
-
-                            </p>
-                            <br />
-                            <p >
-                                <span className='font-bold'>{" Thechnology  "}</span>,
-                                <span className='font-bold'>{" AI "}</span>, and the
-                                <span className='font-bold'>{" cosmos"}</span>,
-                                have an irresistible allure (seductive charm) that captivates my mind.
-                                I find myself endlessly fascinated by the vast wonders of the universe,
-                                the <span className='font-bold'>{" limitless potential "}</span>,
-                                of artificial intelligence,
-                                and the ever-evolving landscape of technology.
-                            </p>
-                            <br />
-                            <p>
-                                When I'm not coding, you'll likely find me
-                                hitting the gym, dabbling in some art,
-                                jet-setting to new destinations, or diving headfirst into learning new languages.
-                                With everything I take on, I bring a mix of creativity, technical skills, and a passion for making the web a more visually appealing and engaging place.
-
-                            </p>
-                            <br />
-                            <div className=' flex justify-between mt-6'>
-
-
-                                <div>
-                                    <ul className='list-none'>
-                                        <li className='flex items-center'>
-                                            <img src="/german.png" alt="German Flag" className="w-4 h-4 rounded-full mr-2" />
-                                            <span className='font-bold'>{"german "}</span> - [fluent]
-                                        </li>
-                                        <br />
-                                        <li className='flex items-center'>
-                                            <img src="/english.png" alt="English Flag" className="w-4 h-4 rounded-full mr-2" />
-                                            <span className='font-bold'>{"english"}</span> - [fluent]
-                                        </li>
-                                        <br /> </ul>
-
-                                </div>
-                                <div>
-                                    <ul className='list-none'>
-                                        <li className='flex items-center'>
-                                            <img src="/japanese.png" alt="Japanese Flag" className="w-4 h-4 rounded-full mr-2" />
-                                            <span className='font-bold'>{"japanese"}</span> - [conversational]
-                                        </li>
-                                        <br />
-                                        <li className='flex items-center'>
-                                            <img src="/hungarian.png" alt="Hungarian Flag" className="w-4 h-4 rounded-full mr-2" />
-                                            <span className='font-bold'>{"hungarian"}</span> - [mothertounge]
-                                        </li>
-                                        <br />
-                                    </ul>
-                                </div>
-
-
-
-
-                            </div>
-
-
-
-
-
-
-                        </div>
-                        <TechStack />
-                        {/*  <section id="techstack" className='  md:w-1/2 '>
+        <div className='my-2 mx-4  md:pt-30 md:pb-30'>
+          <div className='flex flex-col space-y-10 items-stretch justify-center align-top  md:flex-row md:text-left md:p-4 md:space-y-0 md:space-x-10 '>
+            <div className='md:w-1/2'>
+              <h1 className='text-xl2 font-bold mb-6 md:text-left'>
+                Hello there!
+              </h1>
+              <p>
+                {' '}
+                My name is <span className='font-bold'>{'Mate Kamaras'}</span>,
+                and I'm a{' '}
+                <span className='font-bold'>{'Frontend Developer '}</span>, who
+                absolutely loves what I do. Creating awesome digital experiences
+                that blend art, functionality, and user-friendly design is
+                simply awesome{' '}
+              </p>
+              <br />
+              <p>
+                <span className='font-bold'>{' Thechnology  '}</span>,
+                <span className='font-bold'>{' AI '}</span>, and the
+                <span className='font-bold'>{' cosmos'}</span>, have an
+                irresistible allure (seductive charm) that captivates my mind. I
+                find myself endlessly fascinated by the vast wonders of the
+                universe, the{' '}
+                <span className='font-bold'>{' limitless potential '}</span>, of
+                artificial intelligence, and the ever-evolving landscape of
+                technology.
+              </p>
+              <br />
+              <p>
+                When I'm not coding, you'll likely find me hitting the gym,
+                dabbling in some art, jet-setting to new destinations, or diving
+                headfirst into learning new languages. With everything I take
+                on, I bring a mix of creativity, technical skills, and a passion
+                for making the web a more visually appealing and engaging place.
+              </p>
+              <br />
+              <div className=' flex justify-between mt-6'>
+                <div>
+                  <ul className='list-none'>
+                    <li className='flex items-center'>
+                      <Image
+                        src='/german.png'
+                        alt='German Flag'
+                        width={4}
+                        height={4}
+                        className='w-4 h-4 rounded-full mr-2'
+                      />
+                      <span className='font-bold'>{'german '}</span> - [fluent]
+                    </li>
+                    <br />
+                    <li className='flex items-center'>
+                      <Image
+                        src='/english.png'
+                        alt='English Flag'
+                        width={4}
+                        height={4}
+                        className='w-4 h-4 rounded-full mr-2'
+                      />
+                      <span className='font-bold'>{'english'}</span> - [fluent]
+                    </li>
+                    <br />{' '}
+                  </ul>
+                </div>
+                <div>
+                  <ul className='list-none'>
+                    <li className='flex items-center'>
+                      <Image
+                        src='/japanese.png'
+                        alt='Japanese Flag'
+                        width={4}
+                        height={4}
+                        className='w-4 h-4 rounded-full mr-2'
+                      />
+                      <span className='font-bold'>{'japanese'}</span> -
+                      [conversational]
+                    </li>
+                    <br />
+                    <li className='flex items-center'>
+                      <Image
+                        src='/hungarian.png'
+                        alt='Hungarian Flag'
+                        width={4}
+                        height={4}
+                        className='w-4 h-4 rounded-full mr-2'
+                      />
+                      <span className='font-bold'>{'hungarian'}</span> -
+                      [mothertounge]
+                    </li>
+                    <br />
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <TechStack />
+            {/*  <section id="techstack" className='  md:w-1/2 '>
                         <h1 className='text-center text-xl2 font-bold mb-6 md:text-left'>Tech-Stack</h1>
                         <div className='flex flex-col justify-between h-full'>
 
@@ -135,12 +146,11 @@ const AboutSection = () => {
                             </div>
                         </div>
                     </section> */}
-                    </div>
-                </div>
-            </div>
-        </section >
-    )
-}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default AboutSection
-
+export default AboutSection;
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,108 +1,114 @@
-"use client"
-import React from 'react'
-import Image from "next/image"
-import { Link } from 'react-scroll/modules'
-import { HiArrowDown } from "react-icons/hi"
-import { RxDoubleArrowDown } from "react-icons/rx"
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import { Link } from 'react-scroll/modules';
+import { HiArrowDown } from 'react-icons/hi';
+import { RxDoubleArrowDown } from 'react-icons/rx';
 
 const HeroSection = () => {
-    /*border-4 border-solid border-white */
-    return (
-        <section id='home' className='md:mt-40  '>
-            <div className='
+  /*border-4 border-solid border-white */
+  return (
+    <section
+      id='home'
+      className='md:mt-40  '
+    >
+      <div
+        className='
             
-            flex flex-col text-center items-center justify-center mt-20 py-16 md:flex-row md:space-x-4 md:text-left md:pt-35 sm:pt-10'  >
-
-                {/* BLOCK A */}
-                {/*   <div className='flex md:justify-center md:w-1/2 md:mt-2'>
+            flex flex-col text-center items-center justify-center mt-20 py-16 md:flex-row md:space-x-4 md:text-left md:pt-35 sm:pt-10'
+      >
+        {/* BLOCK A */}
+        {/*   <div className='flex md:justify-center md:w-1/2 md:mt-2'>
                     <Image className='rounded-md w-auto h-auto' src="/Mate2.png" alt='headshot' width={300} height={300} priority />
                 </div> */}
-                {/* BLOCK B */}
-                <div className='
-               text-center md:w-3/5 md:mt-2 md:pl-4 justify-center md:border-r-2 md:border-solid md:border-red-500'>
-                    <div className="my-1 animate-bounce  text-gray-800 dark:text-gray-500">
-                        hello konnichiwa szia grüssgott moin,moin!
-                    </div>
-                    <div>
-                        <h1 className='font-bold text-4xl mt-6 md:text-7xl md:mt-0'>
-                            Mate Kamaras</h1>
+        {/* BLOCK B */}
+        <div
+          className='
+               text-center md:w-3/5 md:mt-2 md:pl-4 justify-center md:border-r-2 md:border-solid md:border-red-500'
+        >
+          <div className='my-1 animate-bounce  text-gray-800 dark:text-gray-500'>
+            hello konnichiwa szia grüssgott moin,moin!
+          </div>
+          <div>
+            <h1 className='font-bold text-4xl mt-6 md:text-7xl md:mt-0'>
+              Mate Kamaras
+            </h1>
+          </div>
 
-                    </div>
+          <p className='text-lg mt-4 mb-6 md:tx-2xl'>
+            {' '}
+            <span className='font-semibold text-gray-800 dark:text-gray-500'>
+              Junior Software Engineer
+            </span>{' '}
+            - based in Hamburg, Germany.
+          </p>
+          <Link
+            to='projects'
+            className='text-neutral-100 font-semibold mr-4 rounded-md px-6 py-3 bg-lblue shadow-xl hover:bg-hlblue active:ring-2 active:ring-blue-500 active:ring-offset-2 active:ring-offset-blue-500 cursor-pointer'
+            activeClass='active'
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            View Work
+          </Link>
+          <Link
+            to='techstack'
+            className='text-neutral-100 font-semibold rounded-md px-6 py-3 bg-lblue shadow-xl hover:bg-hlblue active:ring-2 active:ring-blue-500 active:ring-offset-2 active:ring-offset-blue-500 cursor-pointer'
+            activeClass='active'
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Tech Stack
+          </Link>
+        </div>
+        <div className=' flex md:justify-center pt-10  md:w-1/2 md:mt-2'>
+          <Image
+            className='rounded-md shadow-xl w-auto h-auto'
+            src='/Mate2.png'
+            alt='headshot'
+            width={300}
+            height={300}
+            priority
+          />
+        </div>
+      </div>
+      <div className='flex flex-col justify-center  md:mt-40'>
+        <div className='flex justify-evenly '>
+          <Link
+            to='about'
+            activeClass='active'
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            <RxDoubleArrowDown
+              size={40}
+              className='animate-bounce cursor-pointer mb-6 text-gray-400'
+            />
+          </Link>
 
-                    <p className='text-lg mt-4 mb-6 md:tx-2xl'>{" "}
-                        <span className='font-semibold text-gray-800 dark:text-gray-500'>
-                            Junior Software Engineer
-                        </span>
-                        {" "} - based in Hamburg, Germany.
-                    </p>
-                    <Link
-                        to="projects"
-                        className="text-neutral-100 font-semibold mr-4 rounded-md px-6 py-3 bg-lblue shadow-xl hover:bg-hlblue active:ring-2 active:ring-blue-500 active:ring-offset-2 active:ring-offset-blue-500 cursor-pointer"
+          <Link
+            to='about'
+            activeClass='active'
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            <RxDoubleArrowDown
+              size={40}
+              className='animate-bounce cursor-pointer mb-6 text-gray-400'
+            />
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-                        activeClass="active"
-                        spy={true}
-                        smooth={true}
-                        offset={-100}
-                        duration={500}
-                    >
-                        View Work
-                    </Link>
-                    <Link
-                        to="techstack"
-                        className="text-neutral-100 font-semibold rounded-md px-6 py-3 bg-lblue shadow-xl hover:bg-hlblue active:ring-2 active:ring-blue-500 active:ring-offset-2 active:ring-offset-blue-500 cursor-pointer"
-
-                        activeClass="active"
-                        spy={true}
-                        smooth={true}
-                        offset={-100}
-                        duration={500}
-                    >
-                        Tech Stack
-                    </Link>
-
-                </div>
-                <div className=' flex md:justify-center pt-10  md:w-1/2 md:mt-2'>
-                    <Image className='rounded-md shadow-xl w-auto h-auto' src="/Mate2.png" alt='headshot' width={300} height={300} priority />
-                </div>
-            </div>
-            <div className='flex flex-col justify-center  md:mt-40' >
-                <div className='flex justify-evenly '>
-
-                    <Link
-                        to="about"
-                        activeClass="active"
-                        spy={true}
-                        smooth={true}
-                        offset={-100}
-                        duration={500}
-                    >
-                        <RxDoubleArrowDown size={40} className="animate-bounce cursor-pointer mb-6 text-gray-400" />
-
-                    </Link>
-
-                    <Link
-                        to="about"
-                        activeClass="active"
-                        spy={true}
-                        smooth={true}
-                        offset={-100}
-                        duration={500}
-                    >
-                        <RxDoubleArrowDown size={40} className="animate-bounce cursor-pointer mb-6 text-gray-400" />
-
-                    </Link>
-
-                </div>
-
-
-
-
-
-
-
-            </div>
-        </section>
-    )
-}
-
-export default HeroSection
+export default HeroSection;
