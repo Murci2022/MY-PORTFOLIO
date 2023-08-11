86079bc8cc1a93846f79c964a3c4444bd43d1354
MK <Javascript@mates-air.localdomain>
2023-08-11 17:23:19 +0200

    cleaning up and corrections

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -8,39 +8,53 @@ const AboutSection = () => {
       <div className='  break-normal bg-stone-100  dark:bg-black text-gray-800 dark:text-gray-300 py-3 px-3 rounded-xl'>
         <div className='my-2 mx-4  md:pt-30 md:pb-30'>
           <div className='flex flex-col space-y-10 items-stretch justify-center align-top  md:flex-row md:text-left md:p-4 md:space-y-0 md:space-x-10 '>
-            <div className='md:w-1/2 mt-4 md:mt-0'>
-              <h1 className='text-xl2 font-bold mb-6 md:text-left'>
+            <div className='md:w-1/2 mt-4 md:mt-0 dark:text-gray-500'>
+              <h1 className='text-xl2 mb-6 md:text-left text-gray-400'>
                 Hello there!
               </h1>
               <p>
-                {' '}
-                My name is <span className='font-bold'>{'Mate Kamaras'}</span>,
-                and I'm a{' '}
-                <span className='font-bold'>{'Frontend Developer '}</span> who
-                absolutely loves what I do. Creating awesome digital experiences
-                that blend art, functionality, and user-friendly design is
-                simply awesome.{' '}
-              </p>
-              <br />
-              <p>
-                <span className='font-bold'>
-                  {' Thechnology'},{' AI'}
+                My name is
+                <span className=' text-gray-400'>{' Mate Kamaras'}</span>. I'm a
+                <span className=' text-gray-400'>{' Frontend Developer '}</span>
+                who absolutely loves what I do. Creating awesome digital
+                experiences that blends together
+                <span className=' text-gray-400'>{' art'}</span>,
+                <span className=' text-gray-400'>{' functionality'}</span>, and
+                <span className=' text-gray-400'>
+                  {' user-friendly design '}
                 </span>
-                , and the
-                <span className='font-bold'>{' cosmos'}</span>, have an
-                irresistible allure (seductive charm) that captivates my mind. I
-                find myself endlessly fascinated by the{' '}
-                <span className='font-bold'>{' limitless potential'}</span> of
-                artificial intelligence, the ever-evolving landscape of
-                technology and the vast wonders of the universe.
+                is simply awesome.
               </p>
               <br />
               <p>
-                When I'm not coding, you'll likely find me hitting the gym,
-                dabbling in some art, jet-setting to new destinations, or diving
-                headfirst into learning new languages. With everything I take
-                on, I bring a mix of creativity, technical skills, and a passion
-                for making the web a more visually appealing and engaging place.
+                <span className=' text-gray-400'>{'Thechnology, AI '}</span>
+                and the
+                <span className=' text-gray-400'>{' Universe'}</span>, have an
+                irresistible allure that captivates my mind. I find myself
+                endlessly fascinated by the
+                <span className=' text-gray-400'>
+                  {' limitless potential '}
+                </span>
+                of artificial intelligence, the ever-evolving landscape of
+                technology and the vast wonders of the cosmos.
+              </p>
+              <br />
+              <p>
+                <span className=' text-gray-400'>{'When '}</span>
+                I'm
+                <span className=' text-gray-400'>{' not coding'}</span>, you'll
+                likely find me hitting the
+                <span className=' text-gray-400'>{' gym'}</span>, dabbling in
+                some
+                <span className=' text-gray-400'>{' art'}</span>, jet-setting to
+                new destinations, or diving headfirst into learning more
+                <span className=' text-gray-400'>{' japanese'}</span>. With
+                everything I take on, I bring a mix of
+                <span className=' text-gray-400'>{' creativity'}</span>,
+                <span className=' text-gray-400'>{' technical skills '}</span>
+                and a<span className=' text-gray-400'>{' passion for '}</span>
+                making the <span className=' text-gray-400'>{' web '}</span> a
+                more visually appealing and engaging place.
               </p>
               <br />
               <div className=' flex justify-between mt-6'>
